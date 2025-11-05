43070cd28ca9c26a3df6add0d9ae9e55ec97596b
aubert-co <blacket021@gmail.com>
2025-11-04 23:07:22 -0300

    fix: class

--- app/back/src/repository.ts
+++ app/back/src/repository.ts
@@ -1,13 +1,13 @@
 import { Database } from "database/db"
 
-const db = new Database
+const db = new Database()
 type InsertDTO = {
     vacancy_id:number | null,
     source:string
 }
-export const insertAccessLink  =async ({vacancy_id,source}:InsertDTO)=>{
+export const insertAccessLink  = async ({vacancy_id,source}:InsertDTO)=>{
     try{
-        const sql = 'INSERT INTO accessed_links(vacancy_id,source) VALUES($1,$2)'
+        const sql = 'INSERT INTO accessed_links(vacancy_id,source) VALUES($1,$2)';
         await db.query(sql,[vacancy_id,source])
     }catch(err:unknown){
         throw new Error("Failed to insert")
